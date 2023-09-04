79d32bb895f81c1bb7a0306b3030c2daf405c149
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-05 01:45:23 +0200

    **P3PrintingOutput**

--- P1PrintingOutput/Program.cs
+++ P1PrintingOutput/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Output:Hello, World!
+//Output:Welcome to 102 CSharp Basics!
+//Output:It's easy to print output...
+//Output:...to the console!
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+Console.Write("It's easy to print output...\nto the console!\n");
+    
+Console.WriteLine("Did you knew that capital letters matter when typing WriteLine or just Write?");
+
+

343d5fe558811ac8364a18ef335dd5bbe5ae1971
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-05 14:09:25 +0200

    P6DataTypesTaskComplete

--- P6Types/P6Types/Program.cs
+++ P6Types/P6Types/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string outputString ="Hello World";
+Console.WriteLine(outputString);
 
-Console.WriteLine("Hello, World!");
+float decimalValue =0.3f;
+Console.WriteLine(decimalValue);
+
+byte hexByte =0xa7; //I dont understand how this is being calculated, ask the teacher.
+Console.WriteLine(hexByte);

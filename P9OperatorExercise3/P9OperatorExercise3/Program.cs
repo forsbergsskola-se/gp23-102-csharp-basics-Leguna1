bcab774e581c7be8fa71d6f12efeb247f444a003
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 14:43:44 +0200

    P9OperatorsExercise3

---
+++ P9OperatorExercise3/P9OperatorExercise3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("This is mini-modulo calculator.");
+
+Console.WriteLine("Enter the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int remainder = num1 % num2;
+
+
+Console.WriteLine(remainder);

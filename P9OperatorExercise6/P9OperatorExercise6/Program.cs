d1640aeb6dcdcf03630cad2d32b57a8d32d8fa29
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 17:00:16 +0200

    P9OperatorExercise6

---
+++ P9OperatorExercise6/P9OperatorExercise6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Develop a program that takes two integers as input and calculates their product using the multiplication operator.
+
+Console.WriteLine("This is a mini-multiplication calculator.");
+
+Console.WriteLine("Enter the first number: ");
+int num1= int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter the second number: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num1 * num2);
+
+

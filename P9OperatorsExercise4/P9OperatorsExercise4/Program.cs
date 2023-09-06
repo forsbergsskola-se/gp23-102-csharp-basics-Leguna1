641c7e115eb7908cd619ca0c6f0757a8da72f38f
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 15:10:33 +0200

    P9OperatorsExercise4

---
+++ P9OperatorsExercise4/P9OperatorsExercise4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("This is a program that calculates the area of a circle.");
+
+Console.WriteLine("Enter the radius: ");
+float radius = float.Parse(Console.ReadLine());
+
+float area=MathF.PI * radius * radius;
+Console.WriteLine(area); 

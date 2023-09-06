f0ea757a60af9fa9d382847f252bc6daf7b43b74
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 14:19:08 +0200

    P9OperatorExercise2

---
+++ P9OperatorExercise2/P9OperatorExercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("This is mini-division calculator.");
+
+Console.WriteLine("Enter the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+float result = (float)num1 / num2;
+
+Console.WriteLine(result);
+

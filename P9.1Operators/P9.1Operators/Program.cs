f007ff58e0bccaf605ae2e61067baa69060f1619
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 13:46:48 +0200

    P9OperatorExercise1

---
+++ P9.1Operators/P9.1Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Welcome, this is minutes to seconds converter function.");
+
+Console.WriteLine("Give us a number of minutes: ");
+String minute=Console.ReadLine();
+int s = 60;
+int m = int.Parse(minute);
+
+Console.WriteLine(m * s);
+
+
+

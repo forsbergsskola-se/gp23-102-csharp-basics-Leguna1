772208a1b23461d8f5d202da9aecc56217c7f011
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 23:17:02 +0200

    UpdatedVersion

--- P9Operators/P9Operators/Program.cs
+++ P9Operators/P9Operators/Program.cs
@@ -1,16 +1,16 @@
 ﻿using static System.Console;
 
 WriteLine("Give me a number of seconds ");
-double seconds = double.Parse(ReadLine()!);
-double fraction = seconds / 86400;
+var seconds = double.Parse(ReadLine()!);
+var fraction = seconds / 86400;
 
-int days = (int)(seconds / 86400);
+var days = (int)(seconds / 86400);
 seconds %= 86400;
 
-int hours = (int)(seconds / 3600);
+var hours = (int)(seconds / 3600);
 seconds %= 3600;
 
-int minutes = (int)(seconds / 60);
+var minutes = (int)(seconds / 60);
 seconds %= 60;
 
 WriteLine("Seconds: " + seconds);
@@ -19,4 +19,3 @@ WriteLine("Hours: " + hours);
 WriteLine("Days: " + days);
 WriteLine($"{days}.{hours}:{minutes}:{seconds}");
 WriteLine("In total, that's "+fraction+ " Days.");
-

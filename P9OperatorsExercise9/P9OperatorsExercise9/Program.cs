12028861ff9c47044b18731e805ff95bc7feda04
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-06 19:59:54 +0200

    P9OperatorExercise9
    Seconds to minutes converter

---
+++ P9OperatorsExercise9/P9OperatorsExercise9/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a program that takes an integer input representing seconds and converts it to minutes and remaining seconds,
+//then displays the result.
+
+using static System.Console;
+
+WriteLine("Seconds to minutes converter.");
+
+WriteLine("Enter the seconds: ");
+var seconds =float.Parse(ReadLine()!);
+
+var minutes = seconds /60;
+
+WriteLine(minutes +" minutes");
+

5ee19b217ca7dc4029a7b442c51ebaf79671407c
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-07 15:47:37 +0200

    P11IfElse

---
+++ P11IfElse/P11IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("What's your age? ");
+int ageInput= int.Parse(Console.ReadLine()!);
+
+if (ageInput < 13)
+{
+    Console.WriteLine("You are a child");
+}
+else if (ageInput >= 13 && ageInput <= 19)
+{
+    Console.WriteLine("You are a teenager");
+}
+else
+{
+    Console.WriteLine("You are a grown-up");
+}

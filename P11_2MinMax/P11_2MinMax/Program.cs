f06c5abc811ab7b145b754c5ce8eee459bde9c92
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-11 17:07:20 +0200

    P11_2MinMax

---
+++ P11_2MinMax/P11_2MinMax/Program.cs
@@ -0,0 +1,19 @@
+﻿//Asks the user to input three numbers and then displays the minimum and maximum of those numbers.
+
+Console.WriteLine("Enter three number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2 && num1 > num3 && num2 < num3)
+{
+    Console.WriteLine(""+num1+","+num2);
+}
+else if (num2 > num1 && num2 > num3 && num1 > num3)
+{
+    Console.WriteLine(""+num2+","+num3);
+}
+else
+{
+    Console.WriteLine("" +num3+ "," +num1);
+}

59c700c77aeb5729b7b2ac9be600b1c033d64b3d
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-12 00:36:54 +0200

    P11_3Characters

---
+++ P11_3Characters/P11_3Characters/Program.cs
@@ -0,0 +1,16 @@
+﻿//Develop a program that asks the user to enter a single character and
+//then determines whether it's a digit, a vowel or a consonant. Display an appropriate message.
+//  =vowels
+// b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z isConsonant
+
+Console.WriteLine("Enter a letter or a number: ");
+char input = char.Parse(Console.ReadLine()!);
+
+char a, e, i, o, u, A, E, I, O, U;
+
+
+
+
+
+
+

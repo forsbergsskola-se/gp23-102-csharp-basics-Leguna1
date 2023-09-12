25a16342812348cf0192902b21008dfa06077d5f
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-12 16:09:01 +0200

    P12_1Random

---
+++ P12_1Random_Coordinates/P12_1Random_Coordinates/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Boss spawn coordination");
+
+        Random xAxes = new Random();
+        int x= xAxes.Next(0, 100);
+
+        Random yAxes = new Random();
+        int y = yAxes.Next(0, 100);
+
+        Console.WriteLine($"X {x}, " + $"Y {y}");
+    }
+}
+
+

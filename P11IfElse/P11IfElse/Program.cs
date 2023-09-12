d2a1955e99052e1b6590e6c2903d776ce0fb9acb
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-12 18:44:35 +0200

    P11IfElseComplete (Marc version)

--- P11IfElse/P11IfElse/Program.cs
+++ P11IfElse/P11IfElse/Program.cs
@@ -1,15 +1,34 @@
-﻿Console.WriteLine("What's your age? ");
-int ageInput= int.Parse(Console.ReadLine()!);
-
-if (ageInput < 13)
+﻿Console.WriteLine("What's your age?");
+// console input && variable && type
+string ageInput = Console.ReadLine();
+// conversion && variable && type
+int age = int.Parse(ageInput);
+// conditional branching && boolean expressions && comparison operator
+if (age < 13)
 {
-    Console.WriteLine("You are a child");
+    Console.WriteLine("You are a child.");
 }
-else if (ageInput <= 19)
+else if (age <= 19)
 {
-    Console.WriteLine("You are a teenager");
+    Console.WriteLine("You are a teen.");
 }
 else
 {
-    Console.WriteLine("You are a grown-up");
+    Console.WriteLine("You are an adult.");
+}
+
+Console.WriteLine("Give me another integer.");
+string numberInput = Console.ReadLine();
+int number = int.Parse(numberInput);
+
+// number or age (whatever is greater)
+var max = number > age ? number : age;
+Console.WriteLine($"The maximum is {max}.");
+if (max % 2 == 0)
+{
+    Console.WriteLine("It's an even number.");
+}
+else
+{
+    Console.WriteLine("It's an odd number.");
 }

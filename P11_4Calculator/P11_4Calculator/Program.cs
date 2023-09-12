0c37cf8c2c52af19972dab45e834c7292cd7dd50
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-12 18:25:45 +0200

    P11_4Calculator

---
+++ P11_4Calculator/P11_4Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Calculator
+{
+    private static void Main()
+    {
+        Console.WriteLine("Use operators: `+` `-` `*` `/`");
+        Console.WriteLine("Enter a number: ");
+        float num1 = float.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Enter the operator: ");
+        string? symbol = Console.ReadLine();
+
+        Console.WriteLine("Enter another number: ");
+        float num2 = float.Parse(Console.ReadLine()!);
+
+        if (symbol == "+")
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        else if (symbol == "-")
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        else if (symbol == "/")
+        {
+            // Cant divide by zero
+            if (num2 == 0)
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+            }
+            else
+            {
+                Console.WriteLine(num1 / num2);
+            }
+        }
+        else
+        {
+            Console.WriteLine(num1 * num2);
+        }
+    }
+}

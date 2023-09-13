7872e5590bfddfc6cd1465c8a9c2447c71f1ec73
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-13 03:40:23 +0200

    P12_3Random_Item

---
+++ P12_3Random_Item/P12_3Random_Item/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Random Item Drop");
+
+Random random = new Random();
+int randomNumber = random.Next(1, 6);
+
+if (randomNumber == 1)
+{
+    Console.WriteLine("Sword");
+}
+else if (randomNumber ==2)
+{
+    Console.WriteLine("Shield");
+}
+else if (randomNumber == 3)
+{
+    Console.WriteLine("Dagger");
+}
+else if (randomNumber == 4)
+{
+    Console.WriteLine("Bow");
+}
+else
+{
+    Console.WriteLine("Staff");
+}

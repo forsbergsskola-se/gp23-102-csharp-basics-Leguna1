eff15a03a9c09b95e7e0a20029bc76d54e505805
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-09-13 20:53:48 +0200

    NimGameUnfinished

---
+++ Nim/Nim/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Nim!");
+        Console.WriteLine("|||||||||||||||||||||||| (24)");
+
+        int matches = 24;
+        while (matches > 0)
+        {
+
+            // Player's turn
+            PlayerStarts:
+            Console.WriteLine("How many matches do you want to draw?");
+            PlayerDraws:
+            int playerDraw = int.Parse(Console.ReadLine()!);
+
+            if (playerDraw < 1 || playerDraw > 3 || playerDraw > matches)
+            {
+                Console.WriteLine("You can only draw 1-3 matches, try again.");
+                goto PlayerStarts;
+            }
+
+            matches -= playerDraw;
+
+            if (matches == 0 || matches <playerDraw)
+            {
+                Console.WriteLine($"You draw {playerDraw} matches.");
+                Console.WriteLine("Congratulations! You win!");
+                break;
+            }
+
+            Console.WriteLine(new string('|', matches) + $" ({matches})");
+
+            // AI's turn
+            int aiDraw = new Random().Next(1, 4);
+            if (aiDraw < matches)
+            {
+                matches -= aiDraw; 
+                
+                Console.WriteLine($"AI draws {aiDraw} matches.");
+                Console.WriteLine(new string('|', matches) + $" ({matches})");
+                goto PlayerDraws;
+                
+            }
+        }
+    }
+}
